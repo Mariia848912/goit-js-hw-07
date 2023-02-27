6ce448fb030f35998c28dd270b1c39391120c2bf
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-02-27 21:17:48 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const galleryRef = document.querySelector(".gallery");
 const markup = createMarkup(galleryItems);
 galleryRef.innerHTML = markup;
